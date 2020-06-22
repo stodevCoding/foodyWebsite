e61e4970291f02b487367820458f200b8e04e80c
Stodev <anthony.custodio@edu.itescia.fr>
2020-06-22 22:17:53 +0200

    fix spellings mistakes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -23,14 +23,11 @@ width="360"   />
 
   <p><b>Premièrement</b>, nous allons implémenter de la publicité pour générer des revenus stables en corrélation avec l’alimentation.</p>
 
-<p><b>Deuxièmement</b>, un système d’abonnement sera mis en place pour  supprimer la publicité et débloquer différentes options réservées au membre dit  «PREMIUM»  comme la  liste de favoris, et d’autres futurs développements comme l’établissement d’une liste de courses avec les ingrédients des recettes sélectionnées etc.</p>
+<p><b>Deuxièmement</b>, un système d’abonnement sera mis en place pour  supprimer la publicité et débloquer différentes options réservées aux membres dits  «PREMIUM»  comme la  liste de favoris, et d’autres futurs fonctionnalités comme l’établissement d’une liste de courses avec les ingrédients des recettes sélectionnées etc.</p>
 
-<p><b>Et dernier point</b> sur le plus long terme,  nous allons avec nos futurs partenaires  acquis, créer des recettes sponsorisées mettant en avant leurs produits ou ingrédients.</p>
+<p><b>Enfin</b>, à plus long terme,  nous allons avec nos futurs partenaires, créer des recettes sponsorisées qui mettront en avant leurs produits.</p>
     </div>
   </div>
-
-
-
   </div>{" "}
   </div>{" "}
   </Layout>
